3974aab296a7dee7dfbaf875abad512411b63729
DanielWalker <daniel123.dw@gmail.com>
2022-05-26 09:42:27 +1200

    Create calculator app using Jetpack Compose

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.MediumGray
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Calculator()
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun Calculator() {
+    CalculatorTheme {
+        CalculatorTheme {
+            val viewModel = viewModel<CalculatorViewModel>()
+            val state = viewModel.state
+            val buttonSpacing = 8.dp
+            Calculator(
+                state = viewModel.state,
+                onAction = viewModel::onAction,
+                buttonSpacing = buttonSpacing,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MediumGray)
+                    .padding(16.dp)
+            )
+        }
+    }
+}
